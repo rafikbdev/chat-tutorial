c46b3e24efa84cb4fab4ab41cb590f418cc36ab2
Rafik Dev <rafikbdev@gmail.com>
2022-10-06 21:34:08 +0200

    Intergrate channels.

--- mysite/asgi.py
+++ mysite/asgi.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
 
 import os
 
+from channels.routing import ProtocolTypeRouter
+from channels.http import AsgiHandler
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
-application = get_asgi_application()
+application = ProtocolTypeRouter({
+    'http': AsgiHandler(),
+})
